6e8a8f16130cda5701a6c8b86f1f4cc39a70a3d2
Gabriel Basilio <91099260+GaboBas@users.noreply.github.com>
2021-11-03 19:42:25 -0300

    Agregado el controlador de errores y agregado codigo en loadUserByUsername

--- back/src/main/java/com/proyectoegg/libros/controladores/MainController.java
+++ back/src/main/java/com/proyectoegg/libros/controladores/MainController.java
@@ -1,7 +1,9 @@
 package com.proyectoegg.libros.controladores;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequestMapping("/")
@@ -11,4 +13,16 @@ public class MainController {
     public String index(){
         return "index";
     }
+    
+    @GetMapping("/login")
+    public String login(ModelMap model, @RequestParam(required = false) String error, @RequestParam(required = false) String nombre, @RequestParam(required = false) String logout) {
+        if (error != null) {
+            model.addAttribute("error", "El usuario ingresado o la contraseña son incorrectos");
+        }
+        if (nombre != null) {
+            model.addAttribute("nombre", nombre);
+        }
+        return "iniciar-sesion";
+    }
+
 }
