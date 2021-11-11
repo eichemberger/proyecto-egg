f31d86b24503c510fdb1dbd5847968f8c2655875
German Eichemberger <germaneichemberger@gmail.com>
2021-11-11 17:53:30 -0300

    Arreglo libros

--- back/src/main/java/com/proyectoegg/libros/repositorios/LibroRepositorio.java
+++ back/src/main/java/com/proyectoegg/libros/repositorios/LibroRepositorio.java
@@ -1,22 +1,35 @@
 package com.proyectoegg.libros.repositorios;
 
 import com.proyectoegg.libros.entidades.Libro;
+//import java.util.ArrayList;
+import com.proyectoegg.libros.entidades.Materia;
 import com.proyectoegg.libros.entidades.Usuario;
-import java.util.ArrayList;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface LibroRepositorio extends JpaRepository<Libro, String> {
 
-    @Query(value = "SELECT l FROM Libro l WHERE l.usuario = :usuario")
-    ArrayList<Libro> buscarPorUsuario(@Param("usuario") Usuario usuario);
+    @Query(value="SELECT l FROM Libro l WHERE l.usuario = :usuario")
+    ArrayList<Libro> buscarPorUsuarioId(@Param("usuario") Usuario usuario);
 
-    @Query(value = "SELECT l FROM Libro l WHERE l.materia = :materia AND l.usuario = :usuario ")
-    ArrayList<Libro> libroPorMateria(@Param("usuario") Usuario usuario, @Param("materia") String materia);
+    @Query(value="SELECT l FROM Libro l WHERE l.materia = :materia AND l.usuario = :usuario AND l.leido = false AND l.alta = true")
+    ArrayList<Libro> libroPorMateriaSinLeer(@Param("usuario") Usuario usuario, @Param("materia")String materia);
+
+    @Query("SELECT l FROM Libro l WHERE l.leido = true AND l.usuario = :usuario AND l.alta = true")
+    ArrayList<Libro> getLibrosLeidos(@Param("usuario") Usuario usuario);
+
+    @Query("SELECT l FROM Libro l WHERE l.alta = false AND l.usuario = :usuario")
+    ArrayList<Libro> getLibrosEliminados(@Param("usuario") Usuario usuario);
 
     @Modifying
     @Query(value = "DELETE FROM Libro l WHERE l.id= :id")
--- back/src/main/java/com/proyectoegg/libros/servicios/LibroServicio.java
+++ back/src/main/java/com/proyectoegg/libros/servicios/LibroServicio.java
@@ -4,10 +4,9 @@ import com.proyectoegg.libros.entidades.Libro;
 import com.proyectoegg.libros.entidades.Usuario;
 import com.proyectoegg.libros.excepciones.ServiceException;
 import com.proyectoegg.libros.repositorios.LibroRepositorio;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,87 +14,103 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class LibroServicio {
 
-    @Autowired
     private LibroRepositorio libroRepositorio;
+
     @Autowired
-    private UsuarioServicio usuarioServicio;
+    public LibroServicio(LibroRepositorio libroRepositorio) {
+        this.libroRepositorio = libroRepositorio;
+    }
+    // BUSQUEDA
+
+    public List<Libro> buscarPorUsuarioId(Usuario usuario) {
+        return libroRepositorio.buscarPorUsuarioId(usuario);
+    }
+
+    public Libro buscarPorId(String id) {
+        return libroRepositorio.getById(id);
+    }
+
+    // AGREGAR
 
     @Transactional
     public Libro agregarLibro(Libro libro) throws ServiceException {
+        libro.setAlta(true);
+        libro.setLeido(false);
         validar(libro.getTitulo(), libro.getMateria(), libro.getFechaLimite(), libro.getDiasAnticipacion(), libro.getDescripcion());
         return libroRepositorio.save(libro);
     }
 
+    // EDITAR / ELIMINAR UN LIBRO
+
     @Transactional
-    public Libro editarLibro(Libro libro, String idLibro) throws ServiceException {
-        Optional<Libro> resultado = libroRepositorio.findById(idLibro);
+    public Libro editarLibro(Libro libro, String id) throws ServiceException {
+        Libro libroEditar = verificarLibroId(id);
+        validar(libro.getTitulo().trim(), libroEditar.getMateria(), libro.getFechaLimite(), libro.getDiasAnticipacion(), libro.getDescripcion());
+        libroEditar.setTitulo(libro.getTitulo());
+        libroEditar.setAutor(libro.getAutor());
+        libroEditar.setDescripcion(libro.getDescripcion());
+        libroEditar.setFechaLimite(libro.getFechaLimite());
+        libroEditar.setDiasAnticipacion(libro.getDiasAnticipacion());
+        libroEditar.setObligatorio(libro.getObligatorio());
+        libroRepositorio.save(libroEditar);
+        libroRepositorio.flush();
+        return libroEditar;
+    }
+
+    @Transactional
+    public void cambiarAlta(String id) throws ServiceException {
+        Libro libro = verificarLibroId(id);
+        libro.setAlta(!libro.getAlta());
+        libroRepositorio.save(libro);
+    }
+
+    @Transactional
+    public void eliminarDefinitivo(String id) throws ServiceException{
+        verificarLibroId(id);
+        libroRepositorio.eliminarPorId(id);
+    }
+
+    @Transactional
+    public void eliminar(String id) throws ServiceException {
+        Libro libro = verificarLibroId(id);
+        libroRepositorio.delete(libro);
+    }
+
+    @Transactional
+    public void cambiarLeido(String id) throws ServiceException{
+        Libro libro = verificarLibroId(id);
+        libro.setLeido(true);
+        System.out.println(libro.getLeido());
+        libroRepositorio.save(libro);
+    }
+
+    // FILTROS
+
+    public List<Libro> getLibrosMateriaNoLeidos(Usuario usuario, String materia) {
+        return libroRepositorio.libroPorMateriaSinLeer(usuario, materia);
+    }
+
+    public List<Libro> getLibrosEliminados(Usuario usuario) {
+        return libroRepositorio.getLibrosEliminados(usuario);
+    }
+
+    public List<Libro> getLibrosLeidos(Usuario usuario) {
+        return libroRepositorio.getLibrosLeidos(usuario);
+    }
+
+    // VERIFICACIONES
+
+    public Libro verificarLibroId(String id) throws ServiceException {
+        Optional<Libro> resultado = libroRepositorio.findById(id);
         if (resultado.isPresent()) {
-            Libro libroEditar = resultado.get();
-            validarEdit(libro.getTitulo(), libro.getFechaLimite(), libro.getDiasAnticipacion(), libro.getDescripcion());
-            libroEditar.setTitulo(libro.getTitulo());
-            libroEditar.setAutor(libro.getAutor());
-            libroEditar.setDescripcion(libro.getDescripcion());
-            libroEditar.setFechaLimite(libro.getFechaLimite());
-            libroEditar.setDiasAnticipacion(libro.getDiasAnticipacion());
-            libroEditar.setObligatorio(libro.getObligatorio());
-            libroRepositorio.save(libroEditar);
-            libroRepositorio.flush();
-            return libroEditar;
+            return resultado.get();
         } else {
             throw new ServiceException("El libro indicado no se encuentra en el sistema");
         }
     }
 
-    @Transactional
-    public void eliminarSS(String id) {
-        System.out.println(libroRepositorio.getById(id));
-        libroRepositorio.eliminarPorId(id);
-    }
-
-    @Transactional
-    public Libro cambiarLeido(Libro libro) throws ServiceException {
-        if (libro.getLeido()) {
-            libro.setLeido(Boolean.FALSE);
-        } else {
-            libro.setLeido(Boolean.TRUE);
-        }
-
-        return libroRepositorio.save(libro);
-    }
-
-    @Transactional
-    public Libro cambiarAlta(Libro libro) {
-        if (libro.getAlta()) {
-            libro.setAlta(Boolean.FALSE);
-        } else {
-            libro.setAlta(Boolean.TRUE);
-        }
-
-        return libroRepositorio.save(libro);
-    }
-
-    @Transactional
-    public void eliminar(String id) {
-        try {
-            libroRepositorio.eliminarPorId(id);
-        } catch (Exception e) {
-            System.out.println("No se pudo eliminar el libro");
-            System.out.println(e.getMessage());
-        }
-    }
-
-    @Transactional
-    public void eliminar(Libro libro) {
-        try {
-            libroRepositorio.delete(libro);
-        } catch (Exception e) {
-            System.out.println("No se pudo eliminar el libro");
-            System.out.println(e.getMessage());
-        }
-    }
-
     public void validar(String titulo, String materia, Date fechaLimite, Integer diasAnticipacion, String descripcion) throws ServiceException {
-        if (titulo == null || titulo.isEmpty() || titulo.equals(" ")) {
+        if (titulo == null || titulo.trim().isEmpty()) {
             throw new ServiceException("Debe escribir un título");
         }
         if ((fechaLimite.before(new Date())) || (fechaLimite.equals(new Date()))) {
@@ -108,74 +123,9 @@ public class LibroServicio {
         if (descripcion.length() > 250) {
             throw new ServiceException("La descripción debe tener un máximo de 250 caracteres");
         }
-        if (materia == null) {
+        if (materia == null || materia.trim().isEmpty()) {
             throw new ServiceException("Debe ingresar el nombre de la materia a agregar");
         }
-    }
-
-    public void validarEdit(String titulo, Date fechaLimite, Integer diasAnticipacion, String descripcion) throws ServiceException {
-        if (titulo == null || titulo.isEmpty() || titulo.equals(" ")) {
-            throw new ServiceException("Debe escribir un título");
-        }
-        if ((fechaLimite.before(new Date())) || (fechaLimite.equals(new Date()))) {
-            throw new ServiceException("Debe seleccionar una fecha posterior a la actual");
-        }
-        //Esto después vemos bien
-        if (diasAnticipacion < 1) {
-            throw new ServiceException("La cantidad de días debe ser mayor uno");
-        }
-        if (descripcion.length() > 250) {
-            throw new ServiceException("La descripción debe tener un máximo de 250 caracteres");
-        }
-    }
-
-//    @Transactional
-//    public void darDeBaja(Libro libro){
-//        try{
-//            libro.setAlta(false);
-//            libroRepositorio.save(libro);
-//        } catch(Exception e){
-//            System.out.println(e.getMessage());
-//        }
-//        
-//    }
-    
-    //BUSQUEDAS
-    public Libro buscarPorId(String id) {
-        return libroRepositorio.getById(id);
-    }
-
-    public List<Libro> mostrarTodos() {
-        return libroRepositorio.findAll();
-    }
-
-    public ArrayList<Libro> buscarPorUsuarioId(Usuario usuario) {
-        return libroRepositorio.buscarPorUsuario(usuario);
-    }
-
-    public List<Libro> buscarPorMateria(Usuario usuario, String materia) {
-        return libroRepositorio.libroPorMateria(usuario, materia);
-    }
-
-    public ArrayList<Libro> listaLibrosLeidos(Usuario usuario, String materia) {
-        ArrayList<Libro> libros = new ArrayList<>();
-        for (Libro libro : usuario.getLibros()) {
-            if (libro.getLeido() && libro.getMateria().equals(materia)) {
-                libros.add(libro);
-            }
-        }
-        return libros;
-    }
-
-    public ArrayList<Libro> listaLibrosNoLeidos(Usuario usuario, String materia) {
-        ArrayList<Libro> libros = new ArrayList<>();
-        for (Libro libro : usuario.getLibros()) {
-            if (!libro.getLeido() && libro.getMateria().equals(materia) && libro.getAlta()) {
-                libros.add(libro);
-                System.out.println(libro.getAlta());
-            }
-        }
-        return libros;
     }
 
 }
